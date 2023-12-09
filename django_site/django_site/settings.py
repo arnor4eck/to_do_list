0672de0590de1f6201114c8fb06b7bf87e13d0d5
Nikita Kivalin <kivalinnik@mail.ru>
2023-12-09 11:44:25 +0300

    add authenticate by email

--- django_site/django_site/settings.py
+++ django_site/django_site/settings.py
@@ -126,6 +126,7 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 AUTH_USER_MODEL = 'todolist.User'
+AUTHENTICATION_BACKENDS = ('todolist.views.EmailBackend',)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
